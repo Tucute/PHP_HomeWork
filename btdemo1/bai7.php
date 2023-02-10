8d86ab6e49277ce7093d7c09ae647686b39bb1a9
A An Tu <tupnv24@gmail.com>
2023-02-10 08:01:31 +0700

    edit

--- btdemo1/bai7.php
+++ btdemo1/bai7.php
@@ -1,19 +1,19 @@
-<!-- Bài 1: Cho a, viết chương trình in bảng cửu chương của a. 
-    Bài 2: Cho 2 số a,b. Tìm USCLN, BSCNN của 2 số a,b -->
+<!-- Bài 7: Cho a, viết chương trình in bảng cửu chương của a.  -->
 
 <html>
     <body>
-        <?php
-            $n=4;
-            for($i=1;$i<=$n;$i++) {
-                echo "Bảng nhân $i <br>";
-                $count=0;
-                for($j=1; $j<=10; $j++) {
-                    $count = $i*$j;
-                    echo"$i*$j=$count <br>"; 
-                }
-                echo"<br>";
-            }
-        ?>
+        <table border='1 solid'>
+            <tr>
+                <?php
+                    $n=4;
+                    echo"<td>";
+                    for($j=1; $j<=10; $j++) {
+                        echo"$n*$j=".($n*$j);
+                        echo"<br>"; 
+                    }
+                    echo "</td>";
+                ?>
+            </tr>
+        </table>
     </body>
 </html>
